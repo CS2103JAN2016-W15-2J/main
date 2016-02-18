16536e03dad49f696df1662e3b436a601329e209
MunKeat <a0129561@u.nus.edu>
2016-02-18 22:01:20 +0800

    Implemented command line interface
    1. Set the color scheme for command line interface
    2. Added command line which grows to the width of window (suppose window is
    maximized/minimized)
    3. Added "enter" button

--- src/UI/GraphicUserInterface.java
+++ src/UI/GraphicUserInterface.java
@@ -1,8 +1,13 @@
 package UI;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
 
 public class GraphicUserInterface extends Application {
@@ -11,6 +16,21 @@ public class GraphicUserInterface extends Application {
 	public void start(Stage stage) {
 		BorderPane frame = new BorderPane();
 		Scene scene = new Scene(frame);
+		
+		HBox commandLineContainer = new HBox();
+		commandLineContainer.setPadding(new Insets(10, 15, 10, 15));
+		commandLineContainer.setSpacing(10);
+		commandLineContainer.setStyle("-fx-background-color: #1e2123 ;");
+		
+		TextField commandLine = new TextField();
+		commandLine.setStyle("-fx-background-color: #313437; " + "-fx-text-inner-color: #ffffff;");
+		HBox.setHgrow(commandLine, Priority.ALWAYS);
+		
+		Button enterButton = new Button("enter");
+		enterButton.setStyle("-fx-background-color: #313437; " + "-fx-font-weight: bold;" + "-fx-text-fill: #ffffff;");
+		
+		commandLineContainer.getChildren().addAll(commandLine, enterButton);
+		frame.setBottom(commandLineContainer);
 		
 		// Set and show scene
 		stage.setTitle("TrackNote");
