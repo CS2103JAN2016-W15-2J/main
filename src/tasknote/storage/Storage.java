cfa6990d8e9e4eee9c45a3439f00130148f2b091
tharain <won.jr@hotmail.com>
2016-03-01 20:23:30 +0800

    Comments Added To Storage API methods

--- src/tasknote/storage/Storage.java
+++ src/tasknote/storage/Storage.java
@@ -9,14 +9,30 @@ import java.util.ArrayList;
 public class Storage{
 	private FileManipulation fileManipulator;
 	
+	/**
+	 * constructor to construct FileManipulator to manipulate items from/to file
+	 */
 	public Storage(){
 		fileManipulator = new FileManipulation();
 	}
 	
+	/**
+	 * read all the tasks from file and return to logic
+	 * 
+	 * @return ArrayList/<TaskObject/>
+	 * @throws IOException
+	 * @throws TaskListIOException
+	 */
 	public ArrayList<TaskObject> loadTasks() throws IOException, TaskListIOException{
 		return fileManipulator.getTasks();
 	}
 	
+	/**
+	 * write all the tasks from logic into file
+	 * 
+	 * @param overrideTasks
+	 * @throws TaskListIOException
+	 */
 	public void saveTasks(ArrayList<TaskObject> overrideTasks) throws TaskListIOException{
 		fileManipulator.writeTasks(overrideTasks);
 	}
