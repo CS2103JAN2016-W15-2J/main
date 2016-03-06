1a8b4c6c6532cc4484b1b1ede8ea6306b4b10aa7
Girish <girish92_omsairam@yahoo.com.sg>
2016-03-06 23:36:02 +0800

    Changed type of undoCommandType from String to COMMAND_TYPE

--- src/tasknote/logic/History/CommandObject.java
+++ src/tasknote/logic/History/CommandObject.java
@@ -1,22 +1,23 @@
 package tasknote.logic.History;
 
+import tasknote.shared.COMMAND_TYPE;
 import tasknote.shared.TaskObject;
 
 public class CommandObject {
 	
-	private String undoCommandType;
+	private COMMAND_TYPE undoCommandType;
 	private TaskObject taskObject;
 	private int precedingTasks;
 	
 	/******************* CommandObject Constructor *********************/
-	public CommandObject(String undoCommandType, TaskObject taskObject) {
+	public CommandObject(COMMAND_TYPE undoCommandType, TaskObject taskObject) {
 		this.undoCommandType = undoCommandType;
 		this.taskObject = taskObject;
 		this.precedingTasks = 0;
 	}
 	
 	/******************* Accessors *********************/
-	public String getUndoCommandType() {
+	public COMMAND_TYPE getUndoCommandType() {
 		return this.undoCommandType;
 	}
 	
