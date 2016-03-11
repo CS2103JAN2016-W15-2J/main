2e35428227f748a7f90af8f9900947aff8e23800
Girish <girish92_omsairam@yahoo.com.sg>
2016-03-11 10:12:15 +0800

    Added feedback for redo command

--- src/tasknote/shared/Constants.java
+++ src/tasknote/shared/Constants.java
@@ -14,8 +14,10 @@ public class Constants {
 	public static final String MESSAGE_SEARCH_SUCCESSFUL = "Search Successful: %d Result(s) Retrieved";
 	public static final String MESSAGE_UPDATE_SUCCESSFUL = "Task was Successfully Updated";
 	public static final String MESSAGE_UPDATE_UNSUCCESSFUL = "Update Failed";
-	public static final String MESSAGE_UNDONE_SUCCESSFUL = "The Last Operation has been Undone Successfully";
-	public static final String MESSAGE_UNDONE_UNSUCCESSFUL = "Undo Command Failed to be executed Successfully";
+	public static final String MESSAGE_UNDO_SUCCESSFUL = "The Last Operation has been Undone Successfully";
+	public static final String MESSAGE_UNDO_UNSUCCESSFUL = "Undo Command Failed to be executed";
+	public static final String MESSAGE_REDO_SUCCESSFUL = "The Last Operation has been re-done Successfully";
+	public static final String MESSAGE_REDO_UNSUCCESSFUL = "Redo Command Failed to be executed";
 	public static final String MESSAGE_DONE_SUCCESSFUL = "Task \"%s\" has been marked as completed Successfully";
 	public static final String MESSAGE_DONE_UNSUCCESSFUL = "Mark as complete failed";
 	
