275c9e17bf66559c90f2dd5a30bb010e410bfa43
Girish <girish92_omsairam@yahoo.com.sg>
2016-03-11 10:11:10 +0800

    Added redo command class

---
+++ src/tasknote/logic/Commands/RedoCommand.java
@@ -0,0 +1,30 @@
+package tasknote.logic.Commands;
+
+import tasknote.logic.TaskNote;
+import tasknote.shared.Constants;
+
+public class RedoCommand implements Command {
+	
+	private TaskNote taskNote;
+	private String statusOfOperation;
+
+	public RedoCommand(TaskNote taskNote) {
+		this.taskNote = taskNote;
+		statusOfOperation = new String();
+	}
+	
+	public void execute() {
+		statusOfOperation = taskNote.redoLastUndoCommand();
+	}
+
+	public void refreshDisplay() {
+		taskNote.refreshDisplay(taskNote.getTaskList());
+	}
+
+	public String getFeedBack() {
+		if(statusOfOperation.equals("")){
+			statusOfOperation = Constants.ERROR_FEEDBACK;
+		}
+		return statusOfOperation;
+	}
+}
