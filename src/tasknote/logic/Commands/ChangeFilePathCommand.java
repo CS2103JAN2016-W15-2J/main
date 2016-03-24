47000f3039a22049d9f04f1b18fc6a09e1de4597
Girish <girish92_omsairam@yahoo.com.sg>
2016-03-24 23:23:04 +0800

    Added class for change file path command

---
+++ src/tasknote/logic/Commands/ChangeFilePathCommand.java
@@ -0,0 +1,32 @@
+package tasknote.logic.Commands;
+
+import tasknote.logic.TaskNote;
+import tasknote.shared.Constants;
+
+public class ChangeFilePathCommand implements Command {
+
+	private TaskNote taskNote;
+	private String filePath;
+	private String statusOfOperation;
+
+	public ChangeFilePathCommand(TaskNote taskNote, String filePath) {
+		this.filePath = filePath;
+		this.taskNote = taskNote;
+		statusOfOperation = new String();
+	}
+	
+	public void execute() {
+		statusOfOperation = taskNote.changeFilePath(filePath);
+	}
+
+	public void refreshDisplay() {
+		taskNote.refreshDisplay(taskNote.getTaskList());
+	}
+
+	public String getFeedBack() {
+		if(statusOfOperation.equals("")){
+			statusOfOperation = Constants.ERROR_FEEDBACK;
+		}
+		return statusOfOperation;
+	}
+}
