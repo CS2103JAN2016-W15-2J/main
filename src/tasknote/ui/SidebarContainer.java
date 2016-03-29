ed41b4c44b1b045d9b446f0d379cd7ef63f6c715
MunKeat <a0129561@u.nus.edu>
2016-03-30 01:49:12 +0800

    Added Settings tab in SidebarContainer
    1. Refactoring of separator

--- src/tasknote/ui/SidebarContainer.java
+++ src/tasknote/ui/SidebarContainer.java
@@ -5,6 +5,7 @@ import static tasknote.ui.GuiConstant.SPACING_BETWEEN_COMPONENTS;
 import com.pepperonas.fxiconics.FxIconicsLabel;
 import com.pepperonas.fxiconics.MaterialColor;
 import com.pepperonas.fxiconics.cmd.FxFontCommunity.Icons;
+import com.pepperonas.fxiconics.oct.FxFontOcticons;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -22,6 +23,13 @@ public class SidebarContainer extends VBox{
     private ClockContainer _clock = ClockContainer.getInstance();
     private ListView<String> _observableListRepresentation = new ListView<String>();
     private ObservableList<String> _navigationMenu = FXCollections.observableArrayList();
+    private Separator _separator = new Separator();
+    
+    public final String NAVIGATION_TAG_VIEW_ALL = "View All";
+    public final String NAVIGATION_TAG_OUTSTANDING = "Outstanding";
+    public final String NAVIGATION_TAG_OVERDUE = "Overdue";
+    public final String NAVIGATION_TAG_COMPLETED = "Completed";
+    public final String NAVIGATION_TAG_SETTINGS = "Settings";
     
     private SidebarContainer() {
         // Only one instance of SidebarContainer is permitted
@@ -41,10 +49,20 @@ public class SidebarContainer extends VBox{
         return _sidebarContainer;
     }
     
+    /**
+     * getNavigationList() allows user to get the observable
+     * list contained within SidebarContainer.
+     * 
+     * @return          The ObservableList in SidebarContainer.
+     */
     public ListView<String> getNavigationList() {
         return _observableListRepresentation;
     } 
     
+    /**
+     * This method allows the caller to select the 
+     * list item based on the item index.
+     */
     public void selectNavigationCell(int index) {
         if(index > _navigationMenu.size()) {
             return;
@@ -56,12 +74,10 @@ public class SidebarContainer extends VBox{
     private void setupSidebarContainer() {
         setSidebarContainerPresentation();
         setNavigationPresentation();
+        setSeparatorPresentation();
         setNavigationBehaviour();
         
-        Separator s = new Separator();
-        s.setPadding(new Insets(10, 0, 0, 0));
-        
-        this.getChildren().addAll(_clock, s, _observableListRepresentation);
+        this.getChildren().addAll(_clock, _separator, _observableListRepresentation);
     }
     
     private void setSidebarContainerPresentation() {
@@ -69,11 +85,14 @@ public class SidebarContainer extends VBox{
         this.setSpacing(SPACING_BETWEEN_COMPONENTS);
     }
     
+    private void setSeparatorPresentation() {
+        _separator.setId("sidebar-separator");
+    }
+    
     private void setNavigationPresentation() {
         _observableListRepresentation.setId("sidebar-navigation");
-        _navigationMenu.addAll("View All", "Outstanding", "Overdue", "Completed");
+        _navigationMenu.addAll(NAVIGATION_TAG_VIEW_ALL, NAVIGATION_TAG_OUTSTANDING, NAVIGATION_TAG_OVERDUE, NAVIGATION_TAG_COMPLETED, "", NAVIGATION_TAG_SETTINGS);
         _observableListRepresentation.setItems(_navigationMenu);
-        // _observableListRepresentation.setStyle(String.format(PROPERTY_BACKGROUND_COLOR, "#26292c"));
     }
     
     private void setNavigationBehaviour() {
@@ -85,8 +104,10 @@ public class SidebarContainer extends VBox{
                     public void updateItem(String value, boolean empty) {
                         super.updateItem(value, empty);
                         this.getStyleClass().add("sidebar-container");
-                        if (!isEmpty()) {
+                        if (!isEmpty() && !value.isEmpty()) {
                             setGraphic(setNavigationCellPresentation(value));
+                        } else if (!isEmpty() && value.isEmpty()) {
+                            this.setDisable(true);
                         } else {
                             // Prevent duplicate for a single entry
                             setText(null);
@@ -102,25 +123,33 @@ public class SidebarContainer extends VBox{
         HBox box = new HBox();
         box.setPadding(new Insets(0, 0, 0, 20));
         box.setSpacing(20);
-        Icons icon = getIcon(value);
-        FxIconicsLabel btnDefault = (FxIconicsLabel) new FxIconicsLabel.Builder(icon).size(24).color(MaterialColor.GREY_500).build();
-        Label nLabel = new Label(value);
-        // TODO
-        nLabel.getStyleClass().add("sidebar-navigation-cell");
-        box.getChildren().addAll(btnDefault, nLabel);
+        
+        if(!value.isEmpty() && !value.equals(NAVIGATION_TAG_SETTINGS)) {
+            Icons icon = getIcon(value);
+            FxIconicsLabel nvigationIcon = (FxIconicsLabel) new FxIconicsLabel.Builder(icon).size(24).color(MaterialColor.GREY_500).build();
+            Label navigationText = new Label(value);
+            navigationText.getStyleClass().add("sidebar-navigation-cell");
+            box.getChildren().addAll(nvigationIcon, navigationText);
+        } else if (value.equals(NAVIGATION_TAG_SETTINGS)) {
+            com.pepperonas.fxiconics.oct.FxFontOcticons.Icons icon = FxFontOcticons.Icons.oct_gear;
+            FxIconicsLabel nvigationIcon = (FxIconicsLabel) new FxIconicsLabel.Builder(icon).size(24).color(MaterialColor.GREY_500).build();
+            Label navigationText = new Label(value);
+            navigationText.getStyleClass().add("sidebar-navigation-cell");
+            box.getChildren().addAll(nvigationIcon, navigationText);
+        }
         
         return box;
     }
     
     private Icons getIcon(String value) {
         switch(value) {
-            case "View All":
+            case NAVIGATION_TAG_VIEW_ALL:
                 return Icons.cmd_home;
-            case "Outstanding":
+            case NAVIGATION_TAG_OUTSTANDING:
                 return Icons.cmd_alarm_multiple;
-            case "Overdue":
+            case NAVIGATION_TAG_OVERDUE:
                 return Icons.cmd_comment_alert;
-            case "Completed":
+            case NAVIGATION_TAG_COMPLETED:
                 return Icons.cmd_checkbox_multiple_marked;
         }
         
