b6968378493b7cfc45e168a30d938dd4a22d322c
Girish <girish92_omsairam@yahoo.com.sg>
2016-04-01 17:21:02 +0800

    Added UndoneCommand class

---
+++ src/tasknote/logic/Commands/UndoneCommand.java
@@ -0,0 +1,35 @@
+package tasknote.logic.Commands;
+
+import tasknote.logic.TaskNote;
+import tasknote.shared.Constants;
+import tasknote.shared.TaskObject;
+
+public class UndoneCommand implements Command  {
+	
+	private TaskNote taskNote;
+	private TaskObject taskObject;
+	private boolean isComplete;
+	private String statusOfOperation;
+
+	public UndoneCommand(TaskNote taskNote, TaskObject taskObject) {
+		this.taskObject = taskObject;
+		this.taskNote = taskNote;
+		isComplete = false;
+		statusOfOperation = new String();
+	}
+	
+	public void execute() {
+		statusOfOperation = taskNote.setTaskCompletionStatus(taskObject, isComplete);
+	}
+
+	public void refreshDisplay() {
+		taskNote.refreshDisplay(taskNote.getTaskList());
+	}
+
+	public String getFeedBack() {
+		if(statusOfOperation.equals("")){
+			statusOfOperation = Constants.ERROR_FEEDBACK;
+		}
+		return statusOfOperation;
+	}
+}
